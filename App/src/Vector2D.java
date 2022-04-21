4427a4d0869ff3b222151ddf29a188e4f24e8c40
GuusLammers <guuslammers16@gmail.com>
2022-04-21 16:21:52 -0700

    Added helper methods to Vector2D

--- App/src/Vector2D.java
+++ App/src/Vector2D.java
@@ -7,6 +7,46 @@ public class Vector2D {
         this.y = y;
     }
 
+    private void normalize() {
+        /*
+        Normalizes this Vector2D.
+        */
+        double magnitude = this.getMagnitude();
+        this.x = this.x / magnitude;
+        this.y = this.y / magnitude;
+    }
+
+    public void addToVector(Vector2D vector) {
+        /*
+        Adds passed in Vector2D to this Vector2D.
+        */
+        this.x += vector.getX();
+        this.y += vector.getY();
+    }
+
+    public void scaleVector(double scale) {
+        /*
+        Scales this Vector2D by scale.
+        */ 
+        this.x = this.x * scale;
+        this.y = this.y * scale;
+    }
+
+    public double getMagnitude() {
+        /*
+        Returns the magnitude of this Vector2D.
+        */
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    public void setMagnitude(double magnitude) {
+        /*
+        Sets the values of x, and y, in this Vector2D so that the vectors magnitude matches the passed in magnitude.
+        */
+        this.normalize();
+        this.scaleVector(magnitude);
+    }
+
     public double getX() {
         return this.x;
     }
